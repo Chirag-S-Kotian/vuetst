e3cd93ed2da7f21b735bf96e0c235930aab8e78a
Chirag-S-kotian <ckotian117@gmail.com>
2024-07-21 11:21:00 +0530

    "Added script section with data properties, updated template to display dynamic name and conditional paragraph, removed hardcoded h1 text and style"

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
-<template>
-  <h1>Vue jobs</h1>
-</template>
+<script>
+export default {
+  data() {
+    return {
+      name: 'chirag ck',
+      status: false,
+    };
+  },
+},
+</script>
 
-<style scoped>
-h1{
-  color: red;
-}
-</style>
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status">User is active</p>
+</template>
