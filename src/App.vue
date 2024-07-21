2991d365fde608a85926eae94846b1c572323b48
Chirag-S-kotian <ckotian117@gmail.com>
2024-07-21 11:50:56 +0530

    taiwind css added

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,3 @@
-<script>
-export default {
-  data() {
-    return {
-      name: 'chirag ck',
-      status: 'active',
-    };
-  },
-};
-</script>
-
 <template>
-  <h1>{{ name }}</h1>
-  <p v-if="status">User is active</p>
-  <p v-else-if="status === 'inactive'">User is inactive</p>
-  <p v-else>User is not active</p>
+  
 </template>
