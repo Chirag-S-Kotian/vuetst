8106f3e3f806b9e2fc305b4340172160deba68a1
Chirag-S-kotian <ckotian117@gmail.com>
2024-07-21 11:28:15 +0530

    "Update App.vue: change status data type from boolean to string, add v-else-if and v-else conditions to template"

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,15 @@ export default {
   data() {
     return {
       name: 'chirag ck',
-      status: false,
+      status: 'active',
     };
   },
-},
+};
 </script>
 
 <template>
   <h1>{{ name }}</h1>
   <p v-if="status">User is active</p>
+  <p v-else-if="status === 'inactive'">User is inactive</p>
+  <p v-else>User is not active</p>
 </template>
